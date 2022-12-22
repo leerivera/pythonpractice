b62ed978b574e30a34d8b5103ad8e56282f3d140
leerivera <leerivera80@gmail.com>
2022-12-22 11:15:59 -0500

    more

--- test.py
+++ test.py
@@ -4,4 +4,17 @@ float = 5.7
 
 # this is a comment 
 
-print(int_1)
+add = 4 + 11
+
+sub  =  9 - 7
+
+mulit = 6 * 7
+
+expo = 3 ** 8
+
+example = 789
+example2 = 67889
+example3 = 90876
+example4 = True
+
+print(example)
